fb9ed22c8d89b8e8e6cf0c53f105f29d9aec4593
Kiet <kietnguyentuan911@gmail.com>
2025-10-30 22:53:06 +0700

    chores: init project

---
+++ src/test/java/org/httt2/hrms/HrmsApplicationTests.java
@@ -0,0 +1,13 @@
+package org.httt2.hrms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrmsApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
