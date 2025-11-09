e2aa8a4d330e09ba2fedef8a33ef0b67038774df
Kiet <kietnguyentuan911@gmail.com>
2025-11-10 06:59:26 +0700

    fix: grad build

--- src/test/java/org/httt2/hrms/HrmsApplicationTests.java
+++ src/test/java/org/httt2/hrms/HrmsApplicationTests.java
@@ -2,8 +2,10 @@ package org.httt2.hrms;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class HrmsApplicationTests {
 
   @Test
