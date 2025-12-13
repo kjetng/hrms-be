c9dedd5ec0485ae05f12d6ffc5d4e231053bbbb0
Kiet <kietnguyentuan911@gmail.com>
2025-12-13 14:34:06 +0700

    hotfix: load env

--- src/main/java/org/httt2/hrms/HrmsApplication.java
+++ src/main/java/org/httt2/hrms/HrmsApplication.java
@@ -2,8 +2,10 @@ package org.httt2.hrms;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
+@PropertySource("file:${user.dir}/.env")
 public class HrmsApplication {
 
   public static void main(String[] args) {
