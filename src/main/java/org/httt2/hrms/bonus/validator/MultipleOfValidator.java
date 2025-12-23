012c70b6438d02577a0cec0a1c4124596a8a27b1
Nguyễn Chí Lương <ncluong21@clc.fitus.edu.vn>
2025-12-23 20:48:36 +0700

    rollback changes on application.yml

---
+++ src/main/java/org/httt2/hrms/bonus/validator/MultipleOfValidator.java
@@ -0,0 +1,24 @@
+package org.httt2.hrms.bonus.validator;
+
+import jakarta.validation.ConstraintValidator;
+import jakarta.validation.ConstraintValidatorContext;
+import org.httt2.hrms.bonus.validation.annotation.MultipleOf;
+
+public class MultipleOfValidator
+        implements ConstraintValidator<MultipleOf, Number> {
+
+    private long divisor;
+
+    @Override
+    public void initialize(MultipleOf annotation) {
+        this.divisor = annotation.value();
+    }
+
+    @Override
+    public boolean isValid(Number value, ConstraintValidatorContext context) {
+        if (value == null) {
+            return true; // handled by @NotNull
+        }
+        return value.longValue() % divisor == 0;
+    }
+}
